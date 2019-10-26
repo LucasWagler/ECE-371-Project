a5bc1a1ae2e2faf751c1414f3288bbcbdf094e74
Alex Jasper <aj39aj10@gmail.com>
2019-10-26 13:13:41 -0400

    Update IFTTT

--- Code/Python/IFTTT.py
+++ Code/Python/IFTTT.py
@@ -2,9 +2,12 @@
 #10/26/19
 #This program connects to the IFTTT web hook to call a specified phone number.
 #It is intended to run during a security breach.
+
+#In powershell run the following commands:
+#   python -m pip install --upgrade pip
+#   python -m pip install requests
+#   python -m pip install --upgrade ndg-httpsclient
+
 import requests
 
-def notify():
-    requests.post("https://maker.ifttt.com/trigger/breached/with/key/Qzhe5UWU5AUSnoBumev4U")
-
-notify()
+requests.post("https://maker.ifttt.com/trigger/breached/with/key/Qzhe5UWU5AUSnoBumev4U")
